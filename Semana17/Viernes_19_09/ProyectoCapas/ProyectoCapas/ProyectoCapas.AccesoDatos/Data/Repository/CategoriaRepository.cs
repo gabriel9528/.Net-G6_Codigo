f6f95830dcc024eece67e69d190a50cdff7aa3d7
Gabriel <gabrielsxf@gmail.com>
2025-09-19 19:22:23 -0500

    viernes 19_09

---
+++ Semana17/Viernes_19_09/ProyectoCapas/ProyectoCapas/ProyectoCapas.AccesoDatos/Data/Repository/CategoriaRepository.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoCapas.AccesoDatos.Data.Repository.IRepository;
+using ProyectoCapas.Data;
+using ProyectoCapas.Models;
+
+namespace ProyectoCapas.AccesoDatos.Data.Repository
+{
+    public class CategoriaRepository : Repository<Categoria>, ICategoriaRepository
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public CategoriaRepository(ApplicationDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public void Delete(int id)
+        {
+            var objFromDB = _dbContext.Categorias.FirstOrDefault(x => x.Id == id);
+            if (objFromDB != null)
+            {
+                objFromDB.IsActive = false;
+            }
+
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Categoria categoria)
+        {
+            var objFromDB = _dbContext.Categorias.FirstOrDefault(x => x.Id == categoria.Id);
+            if (objFromDB != null)
+            {
+                objFromDB.Nombre = categoria.Nombre;
+                objFromDB.Orden = categoria.Orden;
+            }
+
+            _dbContext.SaveChanges();
+        }
+    }
+}
